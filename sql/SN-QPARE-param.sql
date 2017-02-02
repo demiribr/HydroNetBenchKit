776986d806c1e0a79df3450332ab356997aafa89
Robert Szczepanek <robert@szczepanek.pl>
2017-02-02 14:46:16 +0100

    Typo in SN query

--- sql/SN-QPARE-param.sql
+++ sql/SN-QPARE-param.sql
@@ -1,7 +1,7 @@
 -- stream network model
 -- query for parent
 
-SELECT st.id FROM src_st AS sn,
+SELECT sn.id FROM src_st AS sn,
 (SELECT * FROM src_sn WHERE id={}) node
 WHERE sn.path=subpath(node.path, 0, nlevel(node.path)-1)
     AND sn.root=node.root;
